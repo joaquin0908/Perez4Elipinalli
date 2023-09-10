cb05b2f22e41ad7008751680cf3b85f9b6c65704
joaquin0908 <joaquin79pitu@gmail.com>
2023-09-10 16:28:33 -0300

    lo mismo

--- src/components/PersonajeContainer/PersonajesContainer.jsx
+++ src/components/PersonajeContainer/PersonajesContainer.jsx
@@ -1,30 +1,35 @@
-import React,{ useEffect, useState} from 'react'
-import { GetPersonajes } from '../../api'
+import React, { useEffect, useState } from 'react';
+import { GetPersonajes } from '../../api';
 import Tarjetas from '../Tarjetas/Tarjetas';
 
-
+// Componente para mostrar una lista de personajes
 export default function PersonajesContainer() {
-   
-    const [Personajes, setPersonajes] = useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await GetPersonajes();
-            /* console.log(data) */
-            setPersonajes(data);
-          } catch (error) {
-            console.error('Error al obtener los personajes', error);
-          }
-        };
-    
-        fetchData();
-        //[] se va a renderizar una sola vez 
-      }, []);
-     
+  // Estado para almacenar la lista de personajes
+  const [Personajes, setPersonajes] = useState([]);
+
+  useEffect(() => {
+    // Función para obtener los datos de los personajes
+    const fetchData = async () => {
+      try {
+        const data = await GetPersonajes();
+        // Actualiza el estado con los datos de los personajes obtenidos
+        setPersonajes(data);
+      } catch (error) {
+        console.error('Error al obtener los personajes', error);
+      }
+    };
+
+    // Llamar a fetchData una vez al montar el componente ([] como dependencia)
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {Personajes.map((Personajes)=>(<Tarjetas key={Personajes.id} Data={Personajes} />))}
-      
-    </div> //aca vamos a crear las tarjetas, con la cantidad de pj que tengamos
+      {/* Mapear la lista de personajes y renderizar una tarjeta por cada uno */}
+      {Personajes.map((Personaje) => (
+        <Tarjetas key={Personaje.id} Data={Personaje} />
+      ))}
+    </div>
+    // Aquí se crearán las tarjetas, una por cada personaje en la lista
   );
 }
